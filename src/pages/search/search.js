a87c2182e576537e9325c71bca79a3274b7662d3
ADAllen <1007715170@qq.com>
2019-06-06 20:08:22 +0800

    soclly

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -5,24 +5,37 @@ import axios from 'axios'
 import url from 'js/api.js'
 import qs from 'qs'
 import mixin from 'js/mixin.js'
+import Velocity from 'velocity-animate'
 
 let {keyword,id}=qs.parse(location.search.substr(1))
 new Vue({
     el:'.container',
     data:{
         searchList:null,
-        keyword
-
+        keyword,
+        show:false
     },
     created(){
         this.getSearchList()
+        
     },
     methods:{
         getSearchList(){
             axios.get(url.searchList,{keyword,id}).then(res=>{
                 this.searchList=res.data.lists
             })
-        }
+        },
+        move() {
+            if(document.body.scrollTop > 100) {
+              this.show = true
+            } else {
+              this.show = false
+            }
+          },
+          toTop() {
+            Velocity(document.body, "scroll", { duration: 1000 })
+          }
+        
     },
     mixins:[mixin]
 
