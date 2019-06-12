d0308b6c3396e2152ac2faea4bea3d54e99debf5
ADAllen <1007715170@qq.com>
2019-06-12 23:54:54 +0800

    sku

--- src/pages/goods/goods.js
+++ src/pages/goods/goods.js
@@ -15,6 +15,7 @@ import url from 'js/api.js'
 import axios from 'axios';
 import mixin from 'js/mixin.js'
 import qs from 'qs'
+import Swipe from 'components/Swipe.vue'
 
 let {id}=qs.parse(location.search.substr(1))
 let detailTab=['商品详情','本店成交']
@@ -25,6 +26,9 @@ new Vue ({
         detailTab,
         tabIndex:0,
         dealLists:null,
+        bannerLists:null,
+        skuType:1,
+        showSku:false,
 
     },
     created(){
@@ -35,6 +39,14 @@ new Vue ({
             axios.get(url.details,{id}).then(res=>{
                 
                 this.details=res.data.data
+                this.bannerLists=[]
+                this.details.imgs.forEach(item => {
+                    this.bannerLists.push({
+                        clickUrl:'',
+                        img:item
+                    })
+                    
+                });
             })
         },
         changeTab(index){
@@ -51,6 +63,9 @@ new Vue ({
 
         }
     },
+    components{
+        Swipe
+    }
     mixins:[mixin]
 
 })
